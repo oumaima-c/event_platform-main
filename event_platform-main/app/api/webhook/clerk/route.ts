a83f303d1b1d28132b9d31c91ed3ddf4fc49c571
oumaima-c <mimata5041@gmail.com>
2026-09-05 22:00:32 +0100

    fix: update webhook payload to snake_case

--- event_platform-main/app/api/webhook/clerk/route.ts
+++ event_platform-main/app/api/webhook/clerk/route.ts
@@ -59,7 +59,7 @@ export async function POST(req: Request) {
 
     const user = {
   clerkId: id,
-  email: emailAddresses[0].emailAddress,
+  email: email_addresses[0].emailAddress,
   username: username || "",       // Added fallback just in case
   firstName: first_name || "",    // Fixes the type error
   lastName: last_name || "",      // Fixes the type error
