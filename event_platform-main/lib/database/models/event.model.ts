a8e88270b0cb61f779a2a9b50a3b5e5d068ebc57
oumaima-c <mimata5041@gmail.com>
2026-09-07 10:32:57 +0100

    fix: actually change _id to ObjectId in Event model

--- event_platform-main/lib/database/models/event.model.ts
+++ event_platform-main/lib/database/models/event.model.ts
@@ -1,7 +1,7 @@
 import { Document, Schema, model, models,Types } from "mongoose";
 
 export interface IEvent extends Document {
-  _id: string;
+   _id: Types.ObjectId;
   title: string;
   description?: string;
   location?: string;
