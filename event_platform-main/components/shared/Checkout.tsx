d21b83ca9e38bab454ce5a18f34322ac7a6847c8
oumaima-c <mimata5041@gmail.com>
2026-09-07 10:42:12 +0100

    fix: convert ObjectId to string in Checkout order

--- event_platform-main/components/shared/Checkout.tsx
+++ event_platform-main/components/shared/Checkout.tsx
@@ -23,7 +23,7 @@ const Checkout = ({ event, userId }: { event: IEvent, userId: string }) => {
   const onCheckout = async () => {
     const order = {
       eventTitle: event.title,
-      eventId: event._id,
+      eventId: event._id.toString(),
       price: event.price,
       isFree: event.isFree,
       buyerId: userId
