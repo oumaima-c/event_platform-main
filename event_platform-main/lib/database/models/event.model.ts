8640c65008b7c99d11316923c99c96af5503ed5a
oumaima-c <mimata5041@gmail.com>
2026-09-07 10:28:00 +0100

    fix: update Event model ObjectId types

--- event_platform-main/lib/database/models/event.model.ts
+++ event_platform-main/lib/database/models/event.model.ts
@@ -1,4 +1,4 @@
-import { Document, Schema, model, models } from "mongoose";
+import { Document, Schema, model, models,Types } from "mongoose";
 
 export interface IEvent extends Document {
   _id: string;
